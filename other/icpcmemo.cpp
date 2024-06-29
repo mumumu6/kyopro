7829b74e6b6aa004c7eccc17cb073df393f6e726
mumumu <takumu.sato@gmail.com>
2024-06-30 00:06:08 +0900

    icpc

---
+++ other/icpcmemo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+using ll   = long long;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+#define ft first
+#define sd second
+#define all(x) std::begin(x), std::end(x)
+
+int main() {
+    ofstream outputfile("test.txt");
+    ifstream inputfile("A1"); // 入力ファイル名
+    // rep(i, n) inputfile >> arr[i];
+    // outputfile
+
+    // inputfile.close();
+    outputfile.close();
+}
