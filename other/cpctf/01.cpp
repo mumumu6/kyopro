9b60d331a6aa70cd7a6013dc5e144cb97bc7b37a
mumumu <takumu.sato@gmail.com>
2025-04-18 23:38:37 +0900

    ac : 01

---
+++ other/cpctf/01.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+using ll   = long long;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+bool chmin(auto &a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto &a, auto b) { return a < b ? a = b, 1 : 0; }
+vector<ll> sin45 = {0, 1, 1, 1, 0, -1, -1, -1}, cos45 = {1, 1, 0, -1, -1, -1, 0, 1};
+vector<ll> dx = {0, 1, 0, -1};
+vector<ll> dy = {1, 0, -1, 0};
+#define ft first
+#define sd second
+#define all(x) std::begin(x), std::end(x)
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb(x) push_back(x)
+#define so(z) sort(z.begin(), z.end())
+#define sor(z) sort(z.rbegin(), z.rend())
+#define vec vector<ll>
+#define vecc vector<vector<ll>>
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+
+    ll n;
+    string s;
+    cin >> n >> s;
+
+    // 多分2個前までしか影響しない?
+
+    vec dp(n + 5, 0);
+
+    ll cnt = 0;
+
+    rep(i, n) {
+        if (s[i] == '1') {
+            dp[i + 3] = dp[i + 2];
+        } else {
+            cnt ++;
+            dp[i + 3] = max(dp[i] + 1, dp[i + 1]);// 0をそのままにできる、残せるなら+1、1に変えるならそのまま
+        }
+    }
+
+    cout << cnt - dp[n + 2] << endl;
+}
