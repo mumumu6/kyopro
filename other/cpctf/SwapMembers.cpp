dbc14b490e8909e3d5cfdf05d48df5e2866dd2c4
mumumu <takumu.sato@gmail.com>
2025-04-18 22:47:57 +0900

    ac : 部員並べ替え問題modとmapでクリア

--- other/cpctf/SwapMembers.cpp
+++ other/cpctf/SwapMembers.cpp
@@ -3,23 +3,23 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 using mint = modint998244353;
-using ll = long long;
+using ll   = long long;
 #define rep(i, n) for (ll i = 0; i < (n); i++)
-#define reps(i,a,b) for(ll i = (a); i < (b); i++)
-bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
-bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
-vector<ll> sin45 = { 0, 1, 1, 1, 0, -1, -1, -1 }, cos45 = { 1, 1, 0, -1, -1, -1, 0, 1 };
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+bool chmin(auto &a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto &a, auto b) { return a < b ? a = b, 1 : 0; }
+vector<ll> sin45 = {0, 1, 1, 1, 0, -1, -1, -1}, cos45 = {1, 1, 0, -1, -1, -1, 0, 1};
 vector<ll> dx = {0, 1, 0, -1};
-vector<ll> dy = {1, 0,-1, 0};
+vector<ll> dy = {1, 0, -1, 0};
 #define ft first
 #define sd second
 #define all(x) std::begin(x), std::end(x)
-#define mp(a,b) make_pair(a,b)
-#define pii pair<int,int>
-#define pll pair<ll,ll>
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 #define pb(x) push_back(x)
-#define so(z) sort(z.begin(),z.end())
-#define sor(z) sort(z.rbegin(),z.rend())
+#define so(z) sort(z.begin(), z.end())
+#define sor(z) sort(z.rbegin(), z.rend())
 #define vec vector<ll>
 #define vecc vector<vector<ll>>
 
@@ -28,11 +28,31 @@ int main() {
     ios_base::sync_with_stdio(false);
     cout << fixed << setprecision(20);
 
-    ll n,k;
+    ll n, k;
     cin >> n >> k;
 
     vector<string> s(n);
     vector<string> t(n);
+    bool flag = true;
 
-    
+    map<string, ll> mp;
+
+    rep(i, n) {
+        string a;
+        cin >> a;
+
+        mp[a] = i;
+    }
+
+    rep(i, n) {
+        string a;
+        cin >> a;
+
+        ll id = mp[a];
+
+        if ((id - i) % k != 0) flag = false;
+    }
+
+    if (flag) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
