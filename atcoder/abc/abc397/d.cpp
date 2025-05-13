1683d41c52ae1d79f6af59ceddf300a71fe51688
mumumu <takumu.sato@gmail.com>
2025-05-13 14:06:46 +0900

    abc 397 d n = x^3 - y^3 オーバーフローに気を付ける。範囲に注目する。

--- atcoder/abc/abc397/d.cpp
+++ atcoder/abc/abc397/d.cpp
@@ -3,23 +3,23 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 using mint = modint998244353;
-using ll = long long;
+using ll   = long long;
 #define rep(i, n) for (ll i = 0; i < (n); i++)
-#define reps(i,a,b) for(ll i = (a); i < (b); i++)
-bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
-bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
-vector<ll> sin45 = { 0, 1, 1, 1, 0, -1, -1, -1 }, cos45 = { 1, 1, 0, -1, -1, -1, 0, 1 };
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+bool chmin(auto &a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto &a, auto b) { return a < b ? a = b, 1 : 0; }
+vector<ll> sin45 = {0, 1, 1, 1, 0, -1, -1, -1}, cos45 = {1, 1, 0, -1, -1, -1, 0, 1};
 vector<ll> dx = {0, 1, 0, -1};
-vector<ll> dy = {1, 0,-1, 0};
+vector<ll> dy = {1, 0, -1, 0};
 #define ft first
 #define sd second
 #define all(x) std::begin(x), std::end(x)
-#define mp(a,b) make_pair(a,b)
-#define pii pair<int,int>
-#define pll pair<ll,ll>
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 #define pb(x) push_back(x)
-#define so(z) sort(z.begin(),z.end())
-#define sor(z) sort(z.rbegin(),z.rend())
+#define so(z) sort(z.begin(), z.end())
+#define sor(z) sort(z.rbegin(), z.rend())
 #define vec vector<ll>
 #define vecc vector<vector<ll>>
 
@@ -31,5 +31,36 @@ int main() {
     ll n;
     cin >> n;
 
-    
+    reps(d, 1, 2000000) {
+        ll ok = 1, ng = sqrt(n) + 10;
+        if (n % d != 0) { continue; }
+        ll val;
+        ll mid;
+        while (abs(ok - ng) > 1) {
+            mid = (ok + ng) / 2;
+
+            ll x = mid + d, y = mid;
+            val = x * x + y * y + x * y;
+
+            if (val <= n / d) {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+
+            // if (d == 276544)
+            //     cerr << "ok = " << ok << " ng = " << ng << " mid = " << mid 
+            //           << " val = " << val << " n/d = " << n/ d  <<  "\n";
+        }
+
+        ll x = ok + d, y = ok;
+        val = x * x + y * y + x * y;
+
+        if (val == n / d) {
+            cout << x << " " << y << "\n";
+            return 0;
+        }
+    }
+
+    cout << -1 << "\n";
 }
