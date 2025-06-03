0fb50276a7e8922fa941c9af40ea3658fa0d83a8
mumumu <takumu.sato@gmail.com>
2025-06-03 19:22:46 +0900

    絵具多いと時間オーバーするけどb3

--- atcoder/ahc/048/b2.cpp
+++ atcoder/ahc/048/b2.cpp
@@ -116,7 +116,7 @@ int main() {
 
     auto squ = [](double x) { return x * x; };
 
-    if (t > 19500 && d < 200) {
+    if (t > 19500 && d < 500) {
         rep(ii, n) {
             rep(i, n - 1) {
                 if (i == 0) {
