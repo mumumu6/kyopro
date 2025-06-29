45658dc86f93e4f19ca76b66cdaa0d2a605a7384
mumumu <takumu.sato@gmail.com>
2025-06-30 02:07:35 +0900

    nCrを追加。atocdoerをtemplateから削除

--- atcoder/abc/abc412/a.cpp
+++ atcoder/abc/abc412/a.cpp
@@ -29,26 +29,7 @@ vector<ll> dy = {1, 0, -1, 0};
 #define spa " "
 #define el '\n'
 // ----------------- 階上 ------------------
-mint fact(ll n, bool inv = false) {
-    if (n < 0) return 0; // 負数は0
 
-    static vector<mint> factorial     = {1}; // pair<階乗, 階乗の逆数>
-    static vector<mint> factorial_inv = {1}; // pair<階乗, 階乗の逆数>
-
-    ll siz = factorial.size();
-
-    if (n >= siz) {
-        reps(i, siz, n + 1) factorial.emplace_back(factorial[i - 1] * i);
-        factorial_inv.resize(n + 1);
-        factorial_inv[n] = factorial[n].inv(); // ここだけ逆元をとる。
-        for (ll i = n; i > siz; i--) factorial_inv[i - 1] = factorial_inv[i] * i;
-    }
-
-    return inv ? factorial_inv[n] : factorial[n];
-}
-
-mint perm(ll n, ll k) { return fact(n) * fact(n - k, true); }
-mint comb(ll n, ll k) { return perm(n, k) * fact(k, true); }
 
 // ----------------- オーバーロード -----------------
 template <class T, class U> ostream &operator<<(ostream &os, const pair<T, U> &p) {
