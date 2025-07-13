9f4fd8c60b685367174f99da2d0f933bde92d2ca
mumumu <takumu.sato@gmail.com>
2025-07-13 15:28:21 +0900

    謎環境構築

---
+++ pch/pch.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
--- test.cpp
+++ test.cpp
@@ -1,42 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-//#include <atcoder/all>
-//using namespace atcoder;
-//using mint = modint998244353;
-using ll = long long;
-using i128 = __int128_t;
+#include <atcoder/all>
+using namespace atcoder;
+using mint   = modint998244353;
+using ll     = long long;
+using i128   = __int128_t;
 const ll INF = 4e18;
 #define rep(i, n) for (ll i = 0; i < (n); i++)
-#define reps(i,a,b) for(ll i = (a); i < (b); i++)
-bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
-bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
-vector<ll> sin45 = { 0, 1, 1, 1, 0, -1, -1, -1 }, cos45 = { 1, 1, 0, -1, -1, -1, 0, 1 };
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+bool chmin(auto &a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto &a, auto b) { return a < b ? a = b, 1 : 0; }
+vector<ll> sin45 = {0, 1, 1, 1, 0, -1, -1, -1}, cos45 = {1, 1, 0, -1, -1, -1, 0, 1};
 vector<ll> dx = {0, 1, 0, -1};
-vector<ll> dy = {1, 0,-1, 0};
+vector<ll> dy = {1, 0, -1, 0};
 #define ft first
 #define sd second
 #define all(x) std::begin(x), std::end(x)
-#define mp(a,b) make_pair(a,b)
-#define pii pair<int,int>
-#define pll pair<ll,ll>
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 #define pb(x) push_back(x)
-#define so(z) sort(z.begin(),z.end())
-#define sor(z) sort(z.rbegin(),z.rend())
+#define so(z) sort(z.begin(), z.end())
+#define sor(z) sort(z.rbegin(), z.rend())
 #define vec vector<ll>
 #define vecc vector<vector<ll>>
 #define Yes cout << "Yes" << el
 #define No cout << "No" << el
 #define spa " "
-#define el '\n' 
+#define el '\n'
 
 // ----------------- オーバーロード -----------------
 template <class T, class U> ostream &operator<<(ostream &os, const pair<T, U> &p) {
     return os << '(' << p.first << ", " << p.second << ')';
 }
 
-//template <int MOD> ostream &operator<<(std::ostream &os, const atcoder::static_modint<MOD> &m) {
-//   return os << m.val();
-//}
+// template <int MOD> ostream &operator<<(std::ostream &os, const atcoder::static_modint<MOD> &m) {
+//    return os << m.val();
+// }
 
 template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
     os << '[';
@@ -47,12 +47,12 @@ template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
     return os << ']';
 }
 template <class T> ostream &operator<<(ostream &os, const set<T> &s) {
-os << '{';
-for (auto it = s.begin(); it != s.end(); ++it) {
-    if (it != s.begin()) os << ", ";
-    os << *it;
-}
-return os << '}';
+    os << '{';
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        if (it != s.begin()) os << ", ";
+        os << *it;
+    }
+    return os << '}';
 }
 
 // ----------------- デバッグ用 -----------------
@@ -69,8 +69,8 @@ template <typename... Ts> void impl(const char *names, Ts &&...xs) {
             pos           = i + 1;
         }
     }
-    ll id = 0;
-    auto out  = [&](auto &&v) {
+    ll id    = 0;
+    auto out = [&](auto &&v) {
         cerr << labels[id++] << " = " << v;
         if (id < sizeof...(Ts)) cerr << ",";
     };
@@ -81,16 +81,20 @@ template <typename... Ts> void impl(const char *names, Ts &&...xs) {
 
 #define debug(...) dbg::impl(#__VA_ARGS__, __VA_ARGS__)
 
+bool is_palindrome(const string &s) {
+    int n = s.size();
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[n - 1 - i]) return false;
+    }
+    return true;
+}
+
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     cout << fixed << setprecision(20);
 
-
-    ll a = 0;
-    ll sum = a* 2  + 3;
-
-    sum = sum * 2 + 1;
-
-    cout << sum << el;
+    cout << "Asss" << endl;
+    cout << "aaA" << endl;
+    cout << "" << endl;
 }
