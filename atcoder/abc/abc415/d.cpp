975b85e3d6d0a6b400782bc96fd24aa105aa1d78
mumumu <takumu.sato@gmail.com>
2025-07-19 23:10:27 +0900

    abc415 : 66

---
+++ atcoder/abc/abc415/d.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+using ll     = long long;
+using i128   = __int128_t;
+const ll INF = 4e18;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+#define rrep(i, n) for (ll i = (n) - 1; i >= 0; i--)
+#define rreps(i, a, b) for (ll i = (b) - 1; i >= (a); i--)
+bool chmin(auto &a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto &a, auto b) { return a < b ? a = b, 1 : 0; }
+vector<ll> sin45 = {0, 1, 1, 1, 0, -1, -1, -1}, cos45 = {1, 1, 0, -1, -1, -1, 0, 1};
+vector<ll> dx = {0, 1, 0, -1};
+vector<ll> dy = {1, 0, -1, 0};
+#define ft first
+#define sd second
+#define all(x) std::begin(x), std::end(x)
+#define mp(a, b) make_pair(a, b)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb(x) push_back(x)
+#define so(z) sort(z.begin(), z.end())
+#define sor(z) sort(z.rbegin(), z.rend())
+#define vec vector<ll>
+#define vecc vector<vector<ll>>
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define spa " "
+#define el '\n'
+
+// ----------------- オーバーロード -----------------
+template <class T, class U> ostream &operator<<(ostream &os, const pair<T, U> &p) {
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+
+// template <int MOD> ostream &operator<<(std::ostream &os, const atcoder::static_modint<MOD> &m) {
+//    return os << m.val();
+// }
+
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    os << '[';
+    for (ll i = 0; i < v.size(); ++i) {
+        if (i) os << ", ";
+        os << v[i];
+    }
+    return os << ']';
+}
+template <class T> ostream &operator<<(ostream &os, const set<T> &s) {
+    os << '{';
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        if (it != s.begin()) os << ", ";
+        os << *it;
+    }
+    return os << '}';
+}
+
+// ----------------- デバッグ用 -----------------
+namespace dbg {
+template <typename... Ts> void impl(const char *names, Ts &&...xs) {
+    string_view sv{names};
+    array<string_view, sizeof...(Ts)> labels{};
+    ll pos = 0, idx = 0;
+    for (ll i = 0; i <= sv.size(); ++i) {
+        if (i == sv.size() || sv[i] == ',') {
+            ll start = pos;
+            while (start < i && sv[start] == ' ') ++start; // 前方空白除去
+            labels[idx++] = sv.substr(start, i - start);
+            pos           = i + 1;
+        }
+    }
+    ll id    = 0;
+    auto out = [&](auto &&v) {
+        cerr << labels[id++] << " = " << v;
+        if (id < sizeof...(Ts)) cerr << ",";
+    };
+    (out(std::forward<Ts>(xs)), ...);
+    cerr << '\n';
+}
+} // namespace dbg
+
+#define debug(...) dbg::impl(#__VA_ARGS__, __VA_ARGS__)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+
+    ll n, m;
+    cin >> n >> m;
+
+    priority_queue<pll, vector<pll>, greater<pll>> q;
+
+    rep(i, m) {
+        ll u, v;
+        cin >> u >> v;
+
+        q.push({(u - v), u}); // 減るかず、必要な瓶の数
+    }
+
+    ll ans = 0;
+
+    while (!q.empty()) {
+        auto [d, a] = q.top();
+
+        if (a > n) { // その店は使えない
+            q.pop();
+            continue;
+        }
+
+        // ll ok = 0;
+        // ll ng = n;
+
+        // auto f = [&](ll x) {
+        //     ll t = n - x * d;
+        //     if (t >= 0 && t < a && t + d >= a) return true;
+
+        //     return false;
+        // };
+
+        // while (abs(ok - ng) > 1) {
+        //     ll mid = (ok + ng) / 2;
+
+        //     if (f(mid)) {
+        //         ok = mid;
+        //     } else {
+        //         ng = mid;
+        //     }
+        // }
+
+        ll cnt = (n - a) / d; // 何回使えるか
+        if (n - cnt * d >= a) cnt++;
+        ans += cnt;
+        n -= cnt * d; // 残りの瓶の数
+        // cout << "d: " << d << ", a: " << a << ", cnt: " << cnt << ", n: " << n << el;
+    }
+
+    cout << ans << el;
+}
