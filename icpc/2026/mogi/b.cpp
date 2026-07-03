0ac2fcd74580796bd5b94b6799412b91afd09772
mumumu <takumu.sato@gmail.com>
2026-07-03 12:46:29 +0900

    icpcmogi

---
+++ icpc/2026/mogi/b.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+using ll     = long long;
+using ld     = long double;
+using i128   = __int128_t;
+const ll INF = 4e18;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+#define rrep(i, n) for (ll i = (n) - 1; i >= 0; i--)
+#define rreps(i, a, b) for (ll i = (b) - 1; i >= (a); i--)
+bool chmin(auto &a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto &a, auto b) { return a < b ? a = b, 1 : 0; }
+int popcnt(ll x) { return __builtin_popcountll(x); }
+int popcnt_mod_2(ll x) { return __builtin_parityll(x); }
+int topbit(ll x) { return x == 0 ? -1 : 63 - __builtin_clzll(x); }
+int lowbit(ll x) { return x == 0 ? -1 : __builtin_ctzll(x); }
+
+// pair操作
+template <class T> using P = pair<T, T>;
+#define ft first
+#define sd second
+vector<ll> dx8 = {0, 1, 1, 1, 0, -1, -1, -1}, dy8 = {1, 1, 0, -1, -1, -1, 0, 1};
+vector<ll> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+vector<P<ll>> pdx4 = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+vector<P<ll>> pdx8 = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+template <class T> P<T> operator+(const P<T> &a, const P<T> &b) { return {a.ft + b.ft, a.sd + b.sd}; }
+template <class T> P<T> operator-(const P<T> &a, const P<T> &b) { return {a.ft - b.ft, a.sd - b.sd}; }
+template <class T> P<T> operator-(const P<T> &a) { return {-a.ft, -a.sd}; }
+template <class T, class U> P<T> operator*(const P<T> &a, const U &b) { return {a.ft * b, a.sd * b}; }
+template <class T, class U> P<T> operator/(const P<T> &a, const U &b) { return {a.ft / b, a.sd / b}; }
+template <class T> P<T> &operator+=(P<T> &a, const P<T> &b) { return a = a + b; }
+template <class T> P<T> &operator-=(P<T> &a, const P<T> &b) { return a = a - b; }
+template <class T, class U> P<T> &operator*=(P<T> &a, const U &b) { return a = a * b; }
+template <class T, class U> P<T> &operator/=(P<T> &a, const U &b) { return a = a / b; }
+template <class T> P<T> rotate(const P<T> &a) { return {-a.sd, a.ft}; } // 90 degree ccw
+template <class T> T dot(const P<T> &a, const P<T> &b) { return a.ft * b.ft + a.sd * b.sd; } // 内積
+template <class T> T cross(const P<T> &a, const P<T> &b) { return dot(rotate(a), b); }
+template <class T> T square(const P<T> &a) { return dot(a, a); }
+template <class T> ld norm(const P<T> &a) { return hypotl(a.ft, a.sd); } // aの長さが出る
+template <class T> T gcd(const P<T> &a) { return gcd(a.ft, a.sd); }
+template <class T> P<T> normalize(P<T> a) { // (4,6)　-> (2,3) みたいな感じ
+    if (a == P<T>{}) return a;
+    a /= gcd(a);
+    if (a < P<T>{}) a = -a;
+    return a;
+}
+
+#define all(x) std::begin(x), std::end(x)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb(x) push_back(x)
+#define so(z) sort(z.begin(), z.end())
+#define sor(z) sort(z.rbegin(), z.rend())
+#define vec vector<ll>
+#define vecc vector<vector<ll>>
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define spa " "
+#define el '\n'
+#define overload5(a, b, c, d, e, name, ...) name
+#define each1(i, a) for (auto &&i : a)
+#define each2(x, y, a) for (auto &&[x, y] : a)
+#define each3(x, y, z, a) for (auto &&[x, y, z] : a)
+#define each4(w, x, y, z, a) for (auto &&[w, x, y, z] : a)
+#define each(...) overload5(__VA_ARGS__, each4, each3, each2, each1)(__VA_ARGS__)
+// #define sum(...) accumulate(all(__VA_ARGS__),0LL)
+// #define dsum(...) accumulate(all(__VA_ARGS__),0.0L)
+// #define Msum(...) accumulate(all(__VA_ARGS__),mint{})
+
+struct Inside {
+    ll h, w;
+    Inside(ll h, ll w) : h(h), w(w) {}
+    Inside(P<ll> hw) : h(hw.ft), w(hw.sd) {}
+    bool operator()(ll i, ll j) const { return 0 <= i && i < h && 0 <= j && j < w; }
+    template <class T> bool operator()(P<T> p) const { return (*this)(p.ft, p.sd); }
+};
+
+// ----------------- オーバーロード -----------------
+template <class T, class U> ostream &operator<<(ostream &os, const pair<T, U> &p) {
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+
+// template <int MOD> ostream &operator<<(std::ostream &os, const atcoder::static_modint<MOD> &m) {
+//    return os << m.val();
+// }
+
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    os << '[';
+    for (ll i = 0; i < v.size(); ++i) {
+        if (i) os << ", ";
+        os << v[i];
+    }
+    return os << ']';
+}
+
+template <class T> ostream &operator<<(ostream &os, const vector<vector<T>> &vv) {
+    os << "[\n";
+    for (ll i = 0; i < (ll)vv.size(); ++i) {
+        os << "  " << vv[i];
+        if (i + 1 < (ll)vv.size()) os << '\n';
+    }
+    return os << "\n]";
+}
+template <class T> ostream &operator<<(ostream &os, const set<T> &s) {
+    os << '{';
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        if (it != s.begin()) os << ", ";
+        os << *it;
+    }
+    return os << '}';
+}
+
+template <class K, class V, class Comp, class Alloc>
+ostream &operator<<(ostream &os, const map<K, V, Comp, Alloc> &m) {
+    os << '{';
+    for (auto &[k, v] : m) {
+        if (k != m.begin()->first) os << ", ";
+        os << k << ": " << v;
+    }
+    return os << '}';
+}
+
+// ----------------- デバッグ用 -----------------
+namespace dbg {
+template <typename... Ts> void impl(const char *names, Ts &&...xs) {
+    string_view sv{names};
+    array<string_view, sizeof...(Ts)> labels{};
+    ll pos = 0, idx = 0;
+    for (ll i = 0; i <= sv.size(); ++i) {
+        if (i == sv.size() || sv[i] == ',') {
+            ll start = pos;
+            while (start < i && sv[start] == ' ') ++start; // 前方空白除去
+            labels[idx++] = sv.substr(start, i - start);
+            pos           = i + 1;
+        }
+    }
+    ll id    = 0;
+    auto out = [&](auto &&v) {
+        cerr << labels[id++] << " = " << v;
+        if (id < sizeof...(Ts)) cerr << ", ";
+    };
+    (out(std::forward<Ts>(xs)), ...);
+    cerr << '\n';
+}
+} // namespace dbg
+
+// これをon offする
+#define DEBUG
+
+#ifdef DEBUG
+#define debug(...) dbg::impl(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) ((void)0)
+#endif
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+
+    while (true) {
+        ll n;
+        cin >> n;
+        if (n == 0) break;
+
+        string s;
+        cin >> s;
+
+        set<char> st;
+
+        rep(i, n) {
+            st.insert(s[i]);
+
+            if (st.size() == 2) {
+                swap(s[i - 1], s[i]);
+                break;
+            }
+        }
+
+        if (st.size() == 1) cout << "IMPOSSIBLE" << endl;
+        else cout << s << endl;
+    }
+}
